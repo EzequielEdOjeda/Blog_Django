3b01af112b5dbad75ab07751b2893222b5f62bbf
Ezequiel <149333448+EzequielEdOjeda@users.noreply.github.com>
2025-07-08 13:48:34 -0300

    Update urls.py

--- mi_blog/mi_blog/urls.py
+++ mi_blog/mi_blog/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from usuarios.views import CustomLoginView
 from home.views import HomeView # <--- ¡ASEGÚRATE DE QUE ESTA LÍNEA ESTÉ PRESENTE!
+from django.contrib.auth import views as auth_views
 
 # Personalización del título del sitio de administración
 admin.site.site_header = "Administración del Blog"
@@ -20,6 +21,22 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     # path('', TemplateView.as_view(template_name='home.html'), name='home'), # ELIMINA O COMENTA ESTA LÍNEA
     path('', HomeView.as_view(), name='home'), # AÑADE ESTA LÍNEA
+    # Password reset integrado de Django (puede ir en usuario también)
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset_form.html'
+    ), name='password_reset'),
+
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
