00fccaac8eb25e2eaeea911df2d26a023a60b923
Ezequiel <149333448+EzequielEdOjeda@users.noreply.github.com>
2025-07-08 13:50:45 -0300

    Update settings.py

--- mi_blog/mi_blog/settings.py
+++ mi_blog/mi_blog/settings.py
@@ -143,3 +143,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tuusuario@gmail.com'
+EMAIL_HOST_PASSWORD = 'tupassword-o-app-password'
+DEFAULT_FROM_EMAIL = 'tuusuario@gmail.com'
+
