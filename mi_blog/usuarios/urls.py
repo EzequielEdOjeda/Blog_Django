6e5d1aaba93d2656436ba5ffca032bae6f008f0f
Ezequiel <149333448+EzequielEdOjeda@users.noreply.github.com>
2025-07-10 12:50:34 -0300

    Delete mi_blog/usuarios directory

--- mi_blog/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import CustomPasswordResetView
-
-urlpatterns = [
-    path('register/', views.register, name='register'), # URL para el registro de usuarios
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-]
