8d263cb260828da36a8ec487997790b84051f452
Ezequiel <149333448+EzequielEdOjeda@users.noreply.github.com>
2025-07-11 17:51:03 -0300

    Update urls.py

--- mi_blog/usuarios/urls.py
+++ mi_blog/usuarios/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
 from . import views
-from .views import CustomPasswordResetView
+from django.urls import path
+from .views import (
+    login_view, register_view, CustomPasswordResetView,
+    register, CustomLoginView, inicio  # Asegúrate de tener esta vista
+)
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
     path('register/', views.register, name='register'), # URL para el registro de usuarios
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('', inicio, name='home'),  # Ruta principal
 ]
